b0e76bee51600d09588695f42ebc7bf4778b96cd
darrel2411 <tapsdarrel@gmail.com>
2021-08-20 17:31:00 +0800

    itinerary routes, controller, models done

--- controllers/controllerItinerary.js
+++ controllers/controllerItinerary.js
@@ -1,7 +1,111 @@
-const { ObjectId } = require('mongodb');
-const User = require('../models/User');
+const { ObjectId } = require("mongodb");
+const Itinerary = require("../models/itinerary");
 
 class Controller {
+    static allItineraries(req, res) {
+        const id = {
+            UserId: req.user.id,
+        };
+
+        Itinerary.find(id)
+            .then((data) => {
+                if (data.length > 0) {
+                    res.status(200).json(data);
+                } else {
+                    res.status(404).json({ message: "Itinerary not found" });
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    }
+
+    // static getOne(req, res) {
+    //     const id = {
+    //         _id: ObjectId(req.params.id),
+    //     };
+
+    //     Itinerary.findOne(id)
+    //         .then((data) => {
+    //             if (data) {
+    //                 res.status(200).json(data);
+    //             } else {
+    //                 res.status(404).json({ message: "Itinerary not found" });
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             res.status(500).json(err);
+    //         });
+    // }
+
+    static postItinerary(req, res) {
+        const { id: UserId } = req.user;
+        const { checkIn, checkOut, places, price } = req.body;
+
+        const newItinerary = {
+            UserId,
+            checkIn,
+            checkOut,
+            places,
+            price: +price,
+        };
+
+        Itinerary.insertOne(newItinerary)
+            .then((data) => {
+                // console.log(data);
+                res.status(201).json({ data: newItinerary });
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    }
+
+    static putItinerary(req, res) {
+        const id = {
+            _id: ObjectId(req.params.id),
+        };
+        const { id: UserId } = req.user;
+        const { checkIn, checkOut, places, price } = req.body;
+
+        const updated = {
+            UserId,
+            checkIn,
+            checkOut,
+            places,
+            price: +price,
+        };
+
+        Itinerary.updateOne(id, updated)
+            .then((data) => {
+                if (data.modifiedCount) {
+                    res.status(200).json({ message: "Itinerary has been updated" });
+                } else {
+                    res.status(404).json({ message: "Itinerary not found" });
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    }
+
+    static deleteItinerary(req, res) {
+        const id = {
+            _id: ObjectId(req.params.id),
+        };
+        console.log(id);
+
+        Itinerary.deleteOne(id)
+            .then((data) => {
+                if (data.deletedCount) {
+                    res.status(200).json({ message: `Itinerary with id ${id} has been deleted` });
+                } else {
+                    res.status(404).json({ message: "Itinerary not found" });
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    }
 }
 
-module.exports=Controller;
+module.exports = Controller;
--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,6 +1,13 @@
-const Controller = require('../controllers/controllerUser');
-const router = require('express').Router()
+const Controller = require("../controllers/controllerItinerary");
+const router = require("express").Router();
 
+router.get("/", Controller.allItineraries);
 
+// get one where user login
+// router.get("/", Controller.getOne);
+
+router.post("/", Controller.postItinerary);
+router.put("/:id", Controller.putItinerary);
+router.delete("/:id", Controller.deleteItinerary);
 
 module.exports = router;
