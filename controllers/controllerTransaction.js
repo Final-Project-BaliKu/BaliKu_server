86830c5793f782c8e4e76471e4ea9c9d65c7a07c
mortissimo <78430112+mortissimo@users.noreply.github.com>
2021-08-20 15:12:34 +0700

    Transaction

--- controllers/controllerTransaction.js
+++ controllers/controllerTransaction.js
@@ -2,6 +2,7 @@ const { ObjectId } = require('mongodb');
 const User = require('../models/User');
 
 class Controller {
+    
 }
 
 module.exports=Controller;
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,7 +1,9 @@
 const Controller = require('../controllers/controllerUser');
 const router = require('express').Router()
+const {authentication} = require('../middlewares/auth')
 
-
-
+router.get("/", Controller.allTransaction);
+router.post("/", Controller.postTransaction);
+router.delete("/:id", Controller.deleteTransaction);
 
 module.exports = router;
